4b10efa110f56624d6a00837d3d27c0a17d74a4e
mylanch <lianganchuan@gmail.com>
2017-09-08 14:18:44 +0800

    added install.py

---
+++ install.py
@@ -0,0 +1,18 @@
+#!/bin/sh
+sudo cp -rf sources.list /etc/apt/sources.list
+sudo apt-get update
+sudo git clone https://github.com/mylanch/configuration.git
+sudo chsh -s /bin/zsh
+sudo apt-get -y install curl
+curl -sSL http://acs-public-mirror.oss-cn-hangzhou.aliyuncs.com/docker-ce/internet | sh -
+sudo mkdir -p /etc/docker
+sudo tee /etc/docker/daemon.json <<-'EOF'
+{
+  "registry-mirrors": ["https://4txzd7vs.mirror.aliyuncs.com"]
+}
+EOF
+sudo systemctl daemon-reload
+sudo systemctl restart docker
+sudo apt-get -y install openssh-server
+sudo service ssh start 
+sudo apt-get -y install tree
