4c07ab5b0ae1eb7e8e9827c6aa0871eb22956c4c
mylanch <lianganchuan@gmail.com>
2017-09-08 15:33:46 +0800

    ok

--- install.py
+++ install.py
@@ -1,13 +1,14 @@
 #!/bin/sh
 sudo cp -rf sources.list /etc/apt/sources.list
 sudo apt-get update
+sudo apt-get -y install vim
 sudo cp -rf .vim .oh-my-zsh .vimrc .zshrc ~
 sudo chsh -s /bin/zsh
 sudo apt-get -y install curl
 sudo apt-get -y install apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL http://mirrors.aliyun.com/docker-ce/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] http://mirrors.aliyun.com/docker-ce/linux/ubuntu
-$(lsb_release -cs)
+sudo add-apt-repository "deb [arch=amd64] http://mirrors.aliyun.com/docker-ce/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update
 sudo apt-get -y install docker-ce
 sudo mkdir -p /etc/docker
 sudo tee /etc/docker/daemon.json <<-'EOF'
